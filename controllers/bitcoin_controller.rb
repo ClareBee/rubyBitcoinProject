a6d27fc430c703119aee9117b84265f33156bc3c
Clare <chblackburne@gmail.com>
2018-01-19 20:18:37 +0000

    Successfully parses http response and renders on bitcoin view

--- controllers/bitcoin_controller.rb
+++ controllers/bitcoin_controller.rb
@@ -1,11 +1,24 @@
 require ( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
+require( 'json' )
 require_relative( '../models/bitcoin.rb' )
+require( 'hashie')
+require( 'ostruct' )
 
 get '/bitcoins' do
   @url = 'https://api.coinmarketcap.com/v1/ticker/'
-
   response = RestClient.get(@url)
-  @bitcoins = response.body
+  # creates array of JSON hashes?
+  # @bitcoins = response.body
+  # @bitcoins = JSON.parse(response.body, object_class: OpenStruct)
+
+  # bitcoins_hash = {}
+  # response.body.each do |bitcoin|
+  #   bitcoins_hash[bitcoin] = {name: 'Text'}
+  # @bitcoins = bitcoins_hash
+
+  @bitcoins = JSON.parse(response.body)
+  erb ( :"bitcoins/index")
+
 end
