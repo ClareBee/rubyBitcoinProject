cba54e43c2cdc80fa368457bf2d4d954c3312df0
Clare <chblackburne@gmail.com>
2018-01-20 21:55:50 +0000

    Adds chart data with limited details

--- controllers/graphs_controller.rb
+++ controllers/graphs_controller.rb
@@ -3,11 +3,24 @@ require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative( '../models/bitcoin.rb' )
 require( 'json' )
+require( 'hashie')
+require( 'ostruct')
+
 
 @bitcoins
 get '/graphs' do
-  data = @bitcoins
-  puts data
-  chart = {chart: {type: 'bar', title: 'Bitcoins', data: data, container: 'container'}}
-  erb (:"graphs/index") , locals: {title: 'Anychart Ruby template', chartData: chart.to_json}
+  @url = 'https://api.coinmarketcap.com/v1/ticker/'
+  response = RestClient.get(@url)
+  @bitcoins = JSON.parse(response.body)
+# var chart = anychart.pie([
+#     ['Product A', 1222],
+#     ['Product B', 2431],
+#     ['Product C', 3624],
+#     ['Product D', 5243],
+#     ['Product E', 8813]
+# ]);
+  # chart = {chart: {type: 'bar', title: 'Bitcoins', data: response.body, container: 'container'}}
+  erb (:"graphs/index")
+  # , locals: {title: 'Anychart Ruby template', chartData: chart.to_json}
+
 end
