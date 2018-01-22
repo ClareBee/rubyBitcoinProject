57e4a1d7a636d09b20af5297541c605ee4156710
Clare <chblackburne@gmail.com>
2018-01-22 20:43:25 +0000

    Tweaks margins on graph view

--- controllers/bitcoin_controller.rb
+++ controllers/bitcoin_controller.rb
@@ -1,4 +1,4 @@
-require ( 'sinatra' )
+require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require( 'json' )
@@ -9,14 +9,6 @@ require( 'ostruct' )
 get '/bitcoins' do
   @url = 'https://api.coinmarketcap.com/v1/ticker/'
   response = RestClient.get(@url)
-  # creates array of JSON hashes?
-  # @bitcoins = response.body
-  # @bitcoins = JSON.parse(response.body, object_class: OpenStruct)
-
-  # bitcoins_hash = {}
-  # response.body.each do |bitcoin|
-  #   bitcoins_hash[bitcoin] = {name: 'Text'}
-  # @bitcoins = bitcoins_hash
 
   @bitcoins = JSON.parse(response.body)
   erb ( :"bitcoins/index")
